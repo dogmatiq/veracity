059f7b105e6c90cc8016468f425308e55903ae9a
Danil Petrov <danilpetrovvi@gmail.com>
2022-06-01 20:28:57 +1000

    Ensure AggregateEventStore returns the next unused offset after several writes.

--- persistence/memory/event_test.go
+++ persistence/memory/event_test.go
@@ -54,5 +54,45 @@ var _ = Describe("type AggregateEventStore", func() {
 			Expect(err).ShouldNot(HaveOccurred())
 			Expect(nextOffset).To(BeNumerically("==", 3))
 		})
+
+		It("returns the next unused offset after several writes", func() {
+			err := store.WriteEvents(
+				context.Background(),
+				"<handler>",
+				"<instance>",
+				0,
+				[]*envelopespec.Envelope{
+					{},
+					{},
+					{},
+				},
+				false,
+			)
+
+			Expect(err).ShouldNot(HaveOccurred())
+
+			err = store.WriteEvents(
+				context.Background(),
+				"<handler>",
+				"<instance>",
+				0,
+				[]*envelopespec.Envelope{
+					{},
+					{},
+				},
+				false,
+			)
+
+			Expect(err).ShouldNot(HaveOccurred())
+
+			_, nextOffset, err := store.ReadBounds(
+				context.Background(),
+				"<handler>",
+				"<instance>",
+			)
+
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(nextOffset).To(BeNumerically("==", 5))
+		})
 	})
 })
