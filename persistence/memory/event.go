86cb0f5f5cdd83ad3d1c7a8d5dad63f814455430
Kevin Millar <koden.km@gmail.com>
2022-06-01 20:23:53 +1000

    Ensure AggregateEventStore returns the next unused offset

--- persistence/memory/event.go
+++ persistence/memory/event.go
@@ -2,24 +2,25 @@ package memory
 
 import (
 	"context"
+	"sync"
 
 	"github.com/dogmatiq/interopspec/envelopespec"
 )
 
 // AggregateEventStore stores event messages produced by aggregates
 type AggregateEventStore struct {
+	m          sync.RWMutex
+	nextOffset uint64
 }
 
-// ReadBounds returns "bounds" of the events for a specific aggregate
-// instance.
+// ReadBounds returns "bounds" of the events for a specific aggregate instance.
 //
-// hk is the identity key of the aggregate message handler. id is the
-// aggregate instance ID.
+// hk is the identity key of the aggregate message handler. id is the aggregate
+// instance ID.
 //
-// firstOffset is the offset of the first event recorded by the instance
-// that is considered relevant to its current state. It begins as zero, but
-// is advanced to the offset after the last archived event when events are
-// archived.
+// firstOffset is the offset of the first event recorded by the instance that is
+// considered relevant to its current state. It begins as zero, but is advanced
+// to the offset after the last archived event when events are archived.
 //
 // nextOffset is the offset that will be occupied by the next event to be
 // recorded by this instance.
@@ -27,22 +28,23 @@ func (s *AggregateEventStore) ReadBounds(
 	ctx context.Context,
 	hk, id string,
 ) (firstOffset, nextOffset uint64, _ error) {
-	return 0, 0, nil
+	s.m.RLock()
+	defer s.m.RUnlock()
+
+	return 0, s.nextOffset, nil
 }
 
-// ReadEvents loads some historical events for a specific aggregate
-// instance.
+// ReadEvents loads some historical events for a specific aggregate instance.
 //
-// hk is the identity key of the aggregate message handler. id is the
-// aggregate instance ID.
+// hk is the identity key of the aggregate message handler. id is the aggregate
+// instance ID.
 //
-// If more is true there are more events to be loaded, and ReadEvents()
-// should be called again with firstOffset incremented by len(events).
+// If more is true there are more events to be loaded, and ReadEvents() should
+// be called again with firstOffset incremented by len(events).
 //
 // If more is false there are no subsequent historical events to be loaded.
 //
-// The maximum number of events returned by each call is implementation
-// defined.
+// The maximum number of events returned by each call is implementation defined.
 //
 // It may return an error if firstOffset refers to an archived event.
 func (s *AggregateEventStore) ReadEvents(
@@ -51,4 +53,31 @@ func (s *AggregateEventStore) ReadEvents(
 	firstOffset uint64,
 ) (events []*envelopespec.Envelope, more bool, _ error) {
 	panic("not implemented")
+}
+
+// WriteEvents writes events that were recorded by an aggregate instance.
+//
+// hk is the identity key of the aggregate message handler. id is the aggregate
+// instance ID.
+//
+// nextOffset must be the offset immediately after the offset of the last event
+// written; otherwise, no events are recorded and an error is returned.
+//
+// If archive is true, all prior events and the events being written by this
+// call are archived. Archived events are typically still made available to
+// external event consumers, but will no longer be needed for loading aggregate
+// roots.
+func (s *AggregateEventStore) WriteEvents(
+	ctx context.Context,
+	hk, id string,
+	nextOffset uint64,
+	events []*envelopespec.Envelope,
+	archive bool,
+) error {
+	s.m.Lock()
+	defer s.m.Unlock()
+
+	s.nextOffset += uint64(len(events))
+
+	return nil
 }
--- persistence/memory/event_test.go
+++ persistence/memory/event_test.go
@@ -3,6 +3,7 @@ package memory_test
 import (
 	"context"
 
+	"github.com/dogmatiq/interopspec/envelopespec"
 	. "github.com/dogmatiq/veracity/persistence/memory"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -26,6 +27,32 @@ var _ = Describe("type AggregateEventStore", func() {
 			Expect(err).ShouldNot(HaveOccurred())
 			Expect(firstOffset).To(BeNumerically("==", 0))
 			Expect(nextOffset).To(BeNumerically("==", 0))
+		})
+
+		It("returns the next unused offset", func() {
+			err := store.WriteEvents(
+				context.Background(),
+				"<handler>",
+				"<instance>",
+				0,
+				[]*envelopespec.Envelope{
+					{},
+					{},
+					{},
+				},
+				false,
+			)
+
+			Expect(err).ShouldNot(HaveOccurred())
+
+			_, nextOffset, err := store.ReadBounds(
+				context.Background(),
+				"<handler>",
+				"<instance>",
+			)
+
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(nextOffset).To(BeNumerically("==", 3))
 		})
 	})
 })
