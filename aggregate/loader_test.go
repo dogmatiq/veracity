62579bb971338dcb48b079e5562ca05ea2594ee8
Danil Petrov <danilpetrovvi@gmail.com>
2022-07-09 12:50:08 +1000

    Ensure aggregate.Loader returns an error if event bounds cannot be read.

--- aggregate/loader_test.go
+++ aggregate/loader_test.go
@@ -2,9 +2,12 @@ package aggregate_test
 
 import (
 	"context"
+	"errors"
 
 	"github.com/dogmatiq/configkit"
 	. "github.com/dogmatiq/dogma/fixtures"
+	"github.com/dogmatiq/interopspec/envelopespec"
+
 	. "github.com/dogmatiq/marshalkit/fixtures"
 	. "github.com/dogmatiq/veracity/aggregate"
 	"github.com/dogmatiq/veracity/persistence/memory"
@@ -15,7 +18,7 @@ import (
 var _ = Describe("type Loader", func() {
 	var (
 		handlerID configkit.Identity
-		events    *memory.AggregateEventStore
+		events    *eventReaderStub
 		snapshots *memory.AggregateSnapshotStore
 		root      *AggregateRoot
 		loader    *Loader
@@ -24,7 +27,9 @@ var _ = Describe("type Loader", func() {
 	BeforeEach(func() {
 		handlerID = configkit.MustNewIdentity("<handler-name>", "<handler>")
 
-		events = &memory.AggregateEventStore{}
+		events = &eventReaderStub{
+			EventReader: &memory.AggregateEventStore{},
+		}
 
 		snapshots = &memory.AggregateSnapshotStore{
 			Marshaler: Marshaler,
@@ -33,15 +38,30 @@ var _ = Describe("type Loader", func() {
 		root = &AggregateRoot{}
 
 		loader = &Loader{
-			EventReader:    events,
-			SnapshotWriter: snapshots,
-			SnapshotReader: snapshots,
+			EventReader: events,
 		}
 	})
 
 	Describe("func Load()", func() {
-		XIt("returns an error if event bounds cannot be read", func() {
+		It("returns an error if event bounds cannot be read", func() {
+			events.ReadBoundsFunc = func(
+				ctx context.Context,
+				hk, id string,
+			) (firstOffset uint64, nextOffset uint64, _ error) {
+				return 0, 0, errors.New("<error>")
+			}
 
+			_, _, err := loader.Load(
+				context.Background(),
+				handlerID,
+				"<instance>",
+				root,
+			)
+			Expect(err).To(
+				MatchError(
+					`aggregate root <handler-name>[<instance>] cannot be loaded: unable to read event offset bounds: <error>`,
+				),
+			)
 		})
 
 		When("the instance has no historical events", func() {
@@ -67,6 +87,10 @@ var _ = Describe("type Loader", func() {
 			})
 
 			When("there is a snapshot reader", func() {
+				BeforeEach(func() {
+					loader.SnapshotReader = snapshots
+				})
+
 				XIt("returns an error if the context is cancelled while reading the snapshot", func() {
 
 				})
@@ -91,6 +115,10 @@ var _ = Describe("type Loader", func() {
 			})
 
 			When("there is a snapshot writer", func() {
+				BeforeEach(func() {
+					loader.SnapshotWriter = snapshots
+				})
+
 				XIt("writes a snapshot if the event reader fails after some events are already applied to the root", func() {
 
 				})
@@ -130,3 +158,49 @@ var _ = Describe("type Loader", func() {
 		})
 	})
 })
+
+type eventReaderStub struct {
+	EventReader
+
+	ReadBoundsFunc func(
+		ctx context.Context,
+		hk, id string,
+	) (firstOffset, nextOffset uint64, _ error)
+
+	ReadEventsFunc func(
+		ctx context.Context,
+		hk, id string,
+		firstOffset uint64,
+	) (events []*envelopespec.Envelope, more bool, _ error)
+}
+
+func (s *eventReaderStub) ReadBounds(
+	ctx context.Context,
+	hk, id string,
+) (firstOffset, nextOffset uint64, _ error) {
+	if s.ReadBoundsFunc != nil {
+		return s.ReadBoundsFunc(ctx, hk, id)
+	}
+
+	if s.EventReader != nil {
+		return s.EventReader.ReadBounds(ctx, hk, id)
+	}
+
+	return 0, 0, nil
+}
+
+func (s *eventReaderStub) ReadEvents(
+	ctx context.Context,
+	hk, id string,
+	firstOffset uint64,
+) (events []*envelopespec.Envelope, more bool, _ error) {
+	if s.ReadEventsFunc != nil {
+		return s.ReadEventsFunc(ctx, hk, id, firstOffset)
+	}
+
+	if s.EventReader != nil {
+		return s.EventReader.ReadEvents(ctx, hk, id, firstOffset)
+	}
+
+	return nil, false, nil
+}
