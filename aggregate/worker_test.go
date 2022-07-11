f5b7888899c8280e04d3a756e69e98d08d1809a1
James Harris <contact@jamesharris.id.au>
2022-07-11 18:42:46 +1000

    Add parcel.Packer

--- aggregate/worker_test.go
+++ aggregate/worker_test.go
@@ -5,9 +5,11 @@ import (
 	"time"
 
 	"github.com/dogmatiq/configkit"
+	"github.com/dogmatiq/dogma"
 	. "github.com/dogmatiq/dogma/fixtures"
 	. "github.com/dogmatiq/marshalkit/fixtures"
 	. "github.com/dogmatiq/veracity/aggregate"
+	. "github.com/dogmatiq/veracity/internal/fixtures"
 	"github.com/dogmatiq/veracity/persistence/memory"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -15,6 +17,9 @@ import (
 
 var _ = Describe("type Worker", func() {
 	var (
+		ctx    context.Context
+		cancel context.Cancel
+
 		eventStore  *memory.AggregateEventStore
 		eventReader *eventReaderStub
 		eventWriter *eventWriterStub
@@ -23,12 +28,15 @@ var _ = Describe("type Worker", func() {
 		snapshotReader *snapshotReaderStub
 		snapshotWriter *snapshotWriterStub
 
-		loader  *Loader
-		handler *AggregateMessageHandler
-		worker  *Worker
+		loader   *Loader
+		commands chan Command
+		handler  *AggregateMessageHandler
+		worker   *Worker
 	)
 
 	BeforeEach(func() {
+		ctx, cancel = context.WithTimeout(context.Background(), 5*time.Second)
+
 		eventStore = &memory.AggregateEventStore{}
 
 		eventReader = &eventReaderStub{
@@ -55,7 +63,22 @@ var _ = Describe("type Worker", func() {
 			Marshaler:      Marshaler,
 		}
 
-		handler = &AggregateMessageHandler{}
+		commands = make(chan Command)
+
+		handler = &AggregateMessageHandler{
+			ConfigureFunc: func(c dogma.AggregateConfigurer) {
+				c.Identity("<handler-name>", "<handler-key>")
+				c.ConsumesCommandType(MessageC{})
+				c.ProducesEventType(MessageE{})
+			},
+			HandleCommandFunc: func(
+				r dogma.AggregateRoot,
+				s dogma.AggregateCommandScope,
+				m dogma.Message,
+			) {
+				s.RecordEvent(MessageE{})
+			},
+		}
 
 		worker = &Worker{
 			WorkerConfig: WorkerConfig{
@@ -69,12 +92,29 @@ var _ = Describe("type Worker", func() {
 				SnapshotWriteTimeout: 5 * time.Millisecond,
 				Logger:               nil,
 			},
+			InstanceID: "<instance>",
+			Commands:   commands,
 		}
+	})
+
+	AfterEach(func() {
+		cancel()
 	})
 
 	Describe("func Run()", func() {
 		When("a command is received", func() {
-			XIt("writes events recorded by the handler", func() {
+			It("writes events recorded by the handler", func() {
+				// cmd := Command{
+				// 	Context: context.Background(),
+				// 	Parcel:  NewParcel("<command-1>", MessageC1),
+				// 	Result:  make(chan<- error),
+				// }
+
+				// select {
+				// case <-ctx.Done():
+				// 	Expect(ctx.Err()).ShouldNot(HaveOccurred())
+				// case commands <- cmd:
+				// }
 			})
 
 			XIt("writes a nil error to the command's result channel", func() {
--- parcel/parcel.go
+++ parcel/parcel.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/dogmatiq/dogma"
 	"github.com/dogmatiq/interopspec/envelopespec"
+	"github.com/dogmatiq/marshalkit"
 )
 
 // A Parcel is a container for an envelope and the original information that was
@@ -22,4 +23,41 @@ type Parcel struct {
 	// ScheduledFor is the time at which a timeout message is scheduled to
 	// occur. If the message is not a timeout message it is the zero-value.
 	ScheduledFor time.Time
+}
+
+// ID returns the ID of the message.
+func (p Parcel) ID() string {
+	return p.Envelope.GetMessageId()
+}
+
+// FromEnvelope constructs a parcel from an envelope.
+func FromEnvelope(
+	ma marshalkit.ValueMarshaler,
+	env *envelopespec.Envelope,
+) (Parcel, error) {
+	if err := env.Validate(); err != nil {
+		return Parcel{}, err
+	}
+
+	m, err := marshalkit.UnmarshalMessageFromEnvelope(ma, env)
+	if err != nil {
+		return Parcel{}, err
+	}
+
+	createdAt, err := marshalkit.UnmarshalEnvelopeTime(env.GetCreatedAt())
+	if err != nil {
+		return Parcel{}, err
+	}
+
+	scheduledFor, err := marshalkit.UnmarshalEnvelopeTime(env.GetScheduledFor())
+	if err != nil {
+		return Parcel{}, err
+	}
+
+	return Parcel{
+		Envelope:     env,
+		Message:      m,
+		CreatedAt:    createdAt,
+		ScheduledFor: scheduledFor,
+	}, nil
 }
