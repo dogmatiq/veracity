3c07d79b8a4fb4da618d94f1bbb1a1a8b4cbdb8c
James Harris <contact@jamesharris.id.au>
2022-07-13 11:08:58 +1000

    Ensure `aggregate.Worker` does not return an error if the `SnapshotWriter` is `nil`.

--- aggregate/worker_test.go
+++ aggregate/worker_test.go
@@ -667,7 +667,29 @@ var _ = Describe("type Worker", func() {
 			Expect(called).To(BeTrue())
 		})
 
-		XIt("does not return an error if the SnapshotWriter is nil", func() {
+		It("does not return an error if the SnapshotWriter is nil", func() {
+			// Rely on the fact that a snapshot is taken when the worker shuts
+			// down due to idle timeout.
+			worker.IdleTimeout = 1 * time.Millisecond
+			worker.SnapshotWriter = nil
+
+			snapshotWriter.WriteSnapshotFunc = func(
+				ctx context.Context,
+				hk, id string,
+				r dogma.AggregateRoot,
+				snapshotOffset uint64,
+			) error {
+				panic("unexpected call")
+			}
+
+			executeCommandAsync(
+				ctx,
+				commands,
+				NewParcel("<command>", MessageC1),
+			)
+
+			err := worker.Run(ctx)
+			Expect(err).ShouldNot(HaveOccurred())
 		})
 
 		XIt("returns an error if the root cannot be loaded", func() {
