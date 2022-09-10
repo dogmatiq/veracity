3757c7e57d879fd850aae8aab042e7de11c58ba4
Kevin Millar <kevin@codeworx.com.au>
2022-09-10 14:33:44 +1000

    Ensure Journal.Read() returns false if the version doesn't exist

---
+++ journal/journaltest/test.go
@@ -0,0 +1,44 @@
+package journaltest
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/dogmatiq/veracity/journal"
+)
+
+type TestContext struct {
+	Journal journal.Journal[[]byte]
+	Cleanup func() error
+}
+
+func RunTests(
+	t *testing.T,
+	setup func() TestContext,
+) {
+	t.Run("func Read()", func(t *testing.T) {
+		t.Run("it returns false if the version doesn't exist", func(t *testing.T) {
+			t.Parallel()
+
+			ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+			defer cancel()
+
+			tc := setup()
+			defer func() {
+				if err := tc.Cleanup(); err != nil {
+					t.Fatal(err)
+				}
+			}()
+
+			_, ok, err := tc.Journal.Read(ctx, 1)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if ok {
+				t.Fatal("return non-existent record")
+			}
+		})
+	})
+}
---
+++ journal/memory_test.go
@@ -0,0 +1,19 @@
+package journal_test
+
+import (
+	"testing"
+
+	"github.com/dogmatiq/veracity/journal"
+	"github.com/dogmatiq/veracity/journal/journaltest"
+)
+
+func TestInMemory(t *testing.T) {
+	journaltest.RunTests(t, func() journaltest.TestContext {
+		j := &journal.InMemory[[]byte]{}
+
+		return journaltest.TestContext{
+			Journal: j,
+			Cleanup: j.Close,
+		}
+	})
+}
