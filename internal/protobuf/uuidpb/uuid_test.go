7ef132f1b00d82b5cf4279813c0897c55a9b3dc1
James Harris <contact@jamesharris.id.au>
2023-05-26 13:25:15 +1000

    Use native Go test runner for `uuidpb` package.

--- internal/protobuf/uuidpb/uuid_test.go
+++ internal/protobuf/uuidpb/uuid_test.go
@@ -1,44 +1,58 @@
 package uuidpb_test
 
 import (
+	"bytes"
 	"fmt"
+	"testing"
 
 	. "github.com/dogmatiq/veracity/internal/protobuf/uuidpb"
 	"github.com/google/uuid"
-	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("type UUID", func() {
-	Describe("func FromNative() / ToNative()", func() {
-		It("converts a native UUID to a protobuf UUID and back", func() {
-			expect := uuid.New()
-			pb := FromNative(expect)
-			actual := pb.ToNative()
-			Expect(actual).To(Equal(expect))
-		})
-	})
+func TestUUID_ToNative(t *testing.T) {
+	t.Parallel()
 
-	Describe("func ToString()", func() {
-		expect := uuid.New()
-		pb := FromNative(expect)
-		actual := pb.ToString()
-		Expect(actual).To(Equal(expect.String()))
-	})
+	expect := uuid.New()
+	pb := FromNative(expect)
+	actual := pb.ToNative()
 
-	Describe("func ToBytes()", func() {
-		expect := uuid.New()
-		pb := FromNative(expect)
-		actual := pb.ToBytes()
-		Expect(actual).To(Equal(expect[:]))
-	})
+	if expect != actual {
+		t.Fatalf("got %s, want %s", actual, expect)
+	}
+}
 
-	Describe("func Format()", func() {
-		It("provides a useful string representation", func() {
-			pb := New()
-			expect := pb.ToNative().String()
-			actual := fmt.Sprintf("%s", pb)
-			Expect(actual).To(Equal(expect))
-		})
-	})
-})
+func TestUUID_ToString(t *testing.T) {
+	t.Parallel()
+
+	expect := uuid.New()
+	pb := FromNative(expect)
+	actual := pb.ToString()
+
+	if expect.String() != actual {
+		t.Fatalf("got %s, want %s", actual, expect)
+	}
+}
+
+func TestUUID_ToBytes(t *testing.T) {
+	t.Parallel()
+
+	expect := uuid.New()
+	pb := FromNative(expect)
+	actual := pb.ToBytes()
+
+	if !bytes.Equal(actual, expect[:]) {
+		t.Fatalf("got %#v, want %#v", actual, expect)
+	}
+}
+
+func TestUUID_Format(t *testing.T) {
+	t.Parallel()
+
+	pb := New()
+	expect := pb.ToNative().String()
+	actual := fmt.Sprintf("%s", pb)
+
+	if expect != actual {
+		t.Fatalf("got %#v, want %#v", actual, expect)
+	}
+}
