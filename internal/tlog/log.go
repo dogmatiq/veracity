801eff13b687abaaf0743c6294e1af275c3fb967
James Harris <contact@jamesharris.id.au>
2023-05-31 08:40:54 +1000

    Print attributes as a tree.

--- internal/tlog/log.go
+++ internal/tlog/log.go
@@ -2,7 +2,6 @@ package tlog
 
 import (
 	"context"
-	"fmt"
 	"strings"
 	"testing"
 
@@ -29,40 +28,110 @@ func (h *handler) Enabled(context.Context, slog.Level) bool {
 
 func (h *handler) Handle(_ context.Context, rec slog.Record) error {
 	buf := &strings.Builder{}
+	attrs := slices.Clone(h.attrs)
+
+	if len(h.groups) == 0 {
+		rec.Attrs(func(attr slog.Attr) bool {
+			attrs = append(attrs, attr)
+			return true
+		})
+	} else {
+		var grouped []any
+		rec.Attrs(func(attr slog.Attr) bool {
+			grouped = append(grouped, attr)
+			return true
+		})
+
+		var group slog.Attr
+		for i := len(h.groups) - 1; i >= 0; i-- {
+			group = slog.Group(h.groups[i], grouped...)
+			grouped = []any{group}
+		}
+
+		attrs = append(attrs, group)
+	}
+
+	level := rec.Level.String()
+	buf.WriteString(level)
+
+	if len(attrs) == 0 {
+		buf.WriteString(" ─ ")
+	} else {
+		buf.WriteString(" ┬ ")
+	}
 	buf.WriteString(rec.Message)
 
-	if len(h.attrs) == 0 && rec.NumAttrs() == 0 {
-		return nil
-	}
-
-	buf.WriteString("  ---")
-
-	for _, attr := range h.attrs {
-		buf.WriteString("  ")
-		fmt.Fprintf(buf, "%s: %s", attr.Key, attr.Value)
-	}
-
-	var attrs []any
-	rec.Attrs(func(attr slog.Attr) bool {
-		attrs = append(attrs, attr)
-		return true
-	})
-
-	for i := len(h.groups) - 1; i >= 0; i-- {
-		attrs = []any{
-			slog.Group(h.groups[i], attrs...),
-		}
-	}
-
-	for _, attr := range attrs {
-		a := attr.(slog.Attr)
-		buf.WriteString("  ")
-		fmt.Fprintf(buf, "%s: %s", a.Key, a.Value)
-	}
+	const testLogPrefixWidth = 8
+	indent := len(level) + testLogPrefixWidth
+	writeAttrs(buf, indent, 0, attrs, true)
 
 	h.T.Log(buf.String())
 
 	return nil
+}
+
+func writeAttrs(
+	buf *strings.Builder,
+	indent, depth int,
+	attrs []slog.Attr,
+	lastInParent bool,
+) {
+	if len(attrs) == 0 {
+		return
+	}
+
+	width := 0
+	for _, attr := range attrs {
+		if len(attr.Key) > width {
+			width = len(attr.Key)
+		}
+	}
+
+	seen := map[string]struct{}{}
+
+	for i, attr := range attrs {
+		if _, ok := seen[attr.Key]; ok {
+			continue
+		}
+		seen[attr.Key] = struct{}{}
+
+		last := i == len(attrs)-1
+
+		buf.WriteByte('\n')
+
+		for i := 0; i < indent; i++ {
+			buf.WriteByte(' ')
+		}
+
+		for i := 0; i < depth; i++ {
+			if lastInParent {
+				buf.WriteString("  ")
+			} else {
+				buf.WriteString("│ ")
+			}
+		}
+
+		if last {
+			buf.WriteString("╰─")
+		} else {
+			buf.WriteString("├─")
+		}
+
+		if attr.Value.Kind() == slog.KindGroup {
+			buf.WriteString("┬ ")
+			buf.WriteString(attr.Key)
+			writeAttrs(buf, indent, depth+1, attr.Value.Group(), last)
+		} else {
+			buf.WriteString("─ ")
+			buf.WriteString(attr.Key)
+			buf.WriteString(" ")
+			for i := len(attr.Key); i < width+1; i++ {
+				buf.WriteString("┈")
+			}
+			buf.WriteString(" ")
+			buf.WriteString(attr.Value.String())
+		}
+	}
 }
 
 func (h *handler) WithAttrs(attrs []slog.Attr) slog.Handler {
