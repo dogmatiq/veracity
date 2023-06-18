e8d19d45af616cf0610c620d53c73851aedd74a3
James Harris <contact@jamesharris.id.au>
2023-06-18 10:25:18 +1000

    Implemented basic integration supervisor recovery logic.
    Co-authored-by: Sam <sam.easte@codeworx.com.au>
    Co-authored-by: Kevin Millar <koden.km@gmail.com>
    Co-authored-by: Ben Robey <ben.robey@codeworx.com.au>
    Co-authored-by: Danil Petrov <danilpetrovvi@gmail.com>
    Co-authored-by: Steve Stenzel <steve.stenzel@codeworx.com.au>

--- internal/integration/supervisor.go
+++ internal/integration/supervisor.go
@@ -4,9 +4,12 @@ import (
 	"context"
 
 	"github.com/dogmatiq/dogma"
+	"github.com/dogmatiq/enginekit/protobuf/envelopepb"
 	"github.com/dogmatiq/veracity/internal/envelope"
 	"github.com/dogmatiq/veracity/internal/integration/internal/journalpb"
+	"github.com/dogmatiq/veracity/internal/protobuf/protojournal"
 	"github.com/dogmatiq/veracity/persistence/journal"
+	"golang.org/x/exp/slices"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -16,6 +19,8 @@ type Supervisor struct {
 	HandlerKey     string
 	Journals       journal.Store
 	Packer         *envelope.Packer
+
+	offset uint64
 }
 
 func (s *Supervisor) Run(ctx context.Context) error {
@@ -24,6 +29,40 @@ func (s *Supervisor) Run(ctx context.Context) error {
 		return err
 	}
 	defer j.Close()
+	s.offset = 0
+	pendingCmds := []*envelopepb.Envelope{}
+	if err := protojournal.RangeAll(
+		ctx,
+		j,
+		func(ctx context.Context, offset uint64, record *journalpb.Record) (ok bool, err error) {
+			s.offset = offset + 1
+			if rec := record.GetCommandEnqueued(); rec != nil {
+				cmd := rec.GetCommand()
+				pendingCmds = append(pendingCmds, cmd)
+				return true, nil
+			}
+
+			if rec := record.GetCommandHandled(); rec != nil {
+				for i, cmd := range pendingCmds {
+					if proto.Equal(cmd.GetMessageId(), rec.GetCommandId()) {
+						pendingCmds = slices.Delete(pendingCmds, i, i+1)
+						break
+					}
+				}
+				return true, nil
+			}
+
+			return true, nil
+		},
+	); err != nil {
+		return err
+	}
+
+	for _, cmd := range pendingCmds {
+		if err := s.handleCommand(ctx, cmd, j); err != nil {
+			return err
+		}
+	}
 
 	for {
 		select {
@@ -38,25 +77,50 @@ func (s *Supervisor) Run(ctx context.Context) error {
 				},
 			}
 
-			data, err := proto.Marshal(rec)
-			if err != nil {
+			if err := protojournal.Append(ctx, j, s.offset, rec); err != nil {
 				return err
 			}
 
-			if err := j.Append(ctx, 0, data); err != nil {
-				return err
-			}
-
+			s.offset++
 			close(ex.Done)
 
-			c, err := s.Packer.Unpack(ex.Command)
-			if err != nil {
-				return err
-			}
-
-			if err := s.Handler.HandleCommand(ctx, &scope{}, c); err != nil {
+			if err := s.handleCommand(ctx, ex.Command, j); err != nil {
 				return err
 			}
 		}
 	}
+}
+
+func (s *Supervisor) handleCommand(ctx context.Context, cmd *envelopepb.Envelope, j journal.Journal) error {
+	c, err := s.Packer.Unpack(cmd)
+	if err != nil {
+		return err
+	}
+
+	rec := &journalpb.Record{}
+
+	handlerErr := s.Handler.HandleCommand(ctx, &scope{}, c)
+
+	if handlerErr != nil {
+		rec.OneOf = &journalpb.Record_CommandHandlerFailed{
+			CommandHandlerFailed: &journalpb.CommandHandlerFailed{
+				CommandId: cmd.GetMessageId(),
+				Error:     handlerErr.Error(),
+			},
+		}
+	} else {
+		rec.OneOf = &journalpb.Record_CommandHandled{
+			CommandHandled: &journalpb.CommandHandled{
+				CommandId: cmd.GetMessageId(),
+			},
+		}
+	}
+
+	if err := protojournal.Append(ctx, j, s.offset, rec); err != nil {
+		return err
+	}
+
+	s.offset++
+
+	return handlerErr
 }
--- internal/integration/supervisor_test.go
+++ internal/integration/supervisor_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -17,25 +18,165 @@ import (
 	"github.com/dogmatiq/veracity/persistence/driver/memory"
 )
 
-// - Record command in inbox
-// - Invoke the handler associated with the command
-// - Place all resulting events in the outbox and remove the command from the inbox
-// - Dispatch event 1 in the outbox to the event stream
-// - Contact event stream
-// - Remove event 1 from the outbox
-// - Dispatch event 2 in the outbox to the event stream
-// - Remove event 2 from the outbox
-// - Dispatch event 3 in the outbox to the event stream
-// - Remove event 3 from the outbox
-// - Remove integration from a dirty list.
+// - [x] Record command in inbox
+// - [x] Invoke the handler associated with the command
+// - [ ] Place all resulting events in the outbox and remove the command from the inbox
+// - [ ] Dispatch event 1 in the outbox to the event stream
+// - [ ] Contact event stream
+// - [ ] Remove event 1 from the outbox
+// - [ ] Dispatch event 2 in the outbox to the event stream
+// - [ ] Remove event 2 from the outbox
+// - [ ] Dispatch event 3 in the outbox to the event stream
+// - [ ] Remove event 3 from the outbox
+// - [ ] Remove integration from a dirty list.
 
 func TestSupervisor(t *testing.T) {
-	t.Run("it passes the command to the handler", func(t *testing.T) {
-		packer := &envelope.Packer{
-			Application: identitypb.New("<app>", uuidpb.Generate()),
-			Marshaler:   Marshaler,
+	packer := &envelope.Packer{
+		Application: identitypb.New("<app>", uuidpb.Generate()),
+		Marshaler:   Marshaler,
+	}
+
+	newSupervisor := func() (*integration.Supervisor, *IntegrationMessageHandler, chan *integration.EnqueueCommandExchange) {
+		exchanges := make(chan *integration.EnqueueCommandExchange)
+		handler := &IntegrationMessageHandler{}
+
+		s := &integration.Supervisor{
+			EnqueueCommand: exchanges,
+			HandlerKey:     "faf5be87-e753-407c-a044-e72e4c5bf082",
+			Journals:       &memory.JournalStore{},
+			Packer:         packer,
+			Handler:        handler,
+		}
+		return s, handler, exchanges
+	}
+	t.Run("it does not re-handle successful commands after restart", func(t *testing.T) {
+		env := packer.Pack(MessageC1)
+
+		enqueued := make(chan struct{})
+		ex := &integration.EnqueueCommandExchange{
+			Command: env,
+			Done:    enqueued,
+		}
+		var handledCount atomic.Uint64
+		s, handler, exchanges := newSupervisor()
+		handler.HandleCommandFunc = func(
+			_ context.Context,
+			_ dogma.IntegrationCommandScope,
+			c dogma.Command,
+		) error {
+			handledCount.Add(1)
+			return nil
 		}
 
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		result := make(chan error, 1)
+		go func() {
+			result <- s.Run(ctx)
+		}()
+
+		select {
+		case err := <-result:
+			t.Fatal(err)
+		case exchanges <- ex:
+		}
+
+		select {
+		case err := <-result:
+			t.Fatal(err)
+		case <-enqueued:
+			cancel()
+		}
+
+		err := <-result
+		if !errors.Is(err, context.Canceled) {
+			t.Fatal(err)
+		}
+
+		ctx, cancel = context.WithTimeout(context.Background(), time.Millisecond*250)
+		defer cancel()
+
+		go func() {
+			result <- s.Run(ctx)
+		}()
+
+		err = <-result
+		if !errors.Is(err, context.DeadlineExceeded) {
+			t.Fatal(err)
+		}
+
+		const expectedCalls = 1
+		if handledCount.Load() != expectedCalls {
+			t.Fatalf("unexpected number of calls to handler: got %d, want %d", handledCount.Load(), expectedCalls)
+		}
+	})
+
+	t.Run("it recovers from a handler error", func(t *testing.T) {
+		expectedErr := errors.New("<error>")
+		env := packer.Pack(MessageC1)
+
+		enqueued := make(chan struct{})
+		ex := &integration.EnqueueCommandExchange{
+			Command: env,
+			Done:    enqueued,
+		}
+		handled := make(chan struct{})
+
+		s, handler, exchanges := newSupervisor()
+		handler.HandleCommandFunc = func(
+			_ context.Context,
+			_ dogma.IntegrationCommandScope,
+			c dogma.Command,
+		) error {
+			return expectedErr
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+		result := make(chan error, 1)
+		go func() {
+			result <- s.Run(ctx)
+		}()
+
+		select {
+		case err := <-result:
+			t.Fatal(err)
+		case exchanges <- ex:
+		}
+
+		//TODO: fix race condition
+		select {
+		case err := <-result:
+			t.Fatal(err)
+		case <-enqueued:
+		}
+
+		err := <-result
+		if !errors.Is(err, expectedErr) {
+			t.Fatal(err)
+		}
+
+		handler.HandleCommandFunc = func(ctx context.Context, ics dogma.IntegrationCommandScope, c dogma.Command) error {
+			close(handled)
+			return nil
+		}
+		go func() {
+			result <- s.Run(ctx)
+		}()
+
+		select {
+		case err := <-result:
+			t.Fatal(err)
+		case <-handled:
+			cancel()
+		}
+		err = <-result
+		if !errors.Is(err, context.Canceled) {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("it passes the command to the handler", func(t *testing.T) {
 		env := packer.Pack(MessageC1)
 
 		enqueued := make(chan struct{})
@@ -44,29 +185,21 @@ func TestSupervisor(t *testing.T) {
 			Done:    enqueued,
 		}
 
-		exchanges := make(chan *integration.EnqueueCommandExchange)
 		handled := make(chan struct{})
 
-		s := &integration.Supervisor{
-			EnqueueCommand: exchanges,
-			Handler: &IntegrationMessageHandler{
-				HandleCommandFunc: func(
-					_ context.Context,
-					_ dogma.IntegrationCommandScope,
-					c dogma.Command,
-				) error {
-					close(handled)
+		s, handler, exchanges := newSupervisor()
+		handler.HandleCommandFunc = func(
+			_ context.Context,
+			_ dogma.IntegrationCommandScope,
+			c dogma.Command,
+		) error {
+			close(handled)
 
-					if c != MessageC1 {
-						return fmt.Errorf("unexpected command: got %s, want %s", c, MessageC1)
-					}
+			if c != MessageC1 {
+				return fmt.Errorf("unexpected command: got %s, want %s", c, MessageC1)
+			}
 
-					return nil
-				},
-			},
-			HandlerKey: "faf5be87-e753-407c-a044-e72e4c5bf082",
-			Journals:   &memory.JournalStore{},
-			Packer:     packer,
+			return nil
 		}
 
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
