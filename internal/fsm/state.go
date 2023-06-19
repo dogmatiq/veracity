239ba51f83f7f13af8129c206a70d330c53478bb
James Harris <contact@jamesharris.id.au>
2023-06-19 14:10:16 +1000

    Add "previous" state to FSM.

--- internal/fsm/state.go
+++ internal/fsm/state.go
@@ -7,7 +7,7 @@ type State func(context.Context) (Action, error)
 
 // Run runs the state machine until it is stopped or an error occurs.
 func Run(ctx context.Context, initial State) error {
-	m := &fsm{initial}
+	m := &fsm{initial, nil}
 
 	for m.current != nil {
 		act, err := m.current(ctx)
@@ -22,37 +22,54 @@ func Run(ctx context.Context, initial State) error {
 }
 
 type fsm struct {
-	current State
+	current, previous State
 }
 
 // Action describes the action taken by a state.
 type Action func(*fsm)
 
-// StayInCurrentState is an action that stays in the current state.
-func StayInCurrentState(*fsm) {
+// Stay is an action that stays in the current state.
+func Stay() Action {
+	return stay
 }
 
-// Stop is an action that stops the state machine.
-func Stop(m *fsm) {
+func stay(m *fsm) {}
+
+// Exit is an action that stops the state machine.
+func Exit() Action {
+	return exit
+}
+
+func exit(m *fsm) {
 	m.current = nil
+}
+
+// Back is an action that transitions to the previous state.
+func Back() Action {
+	return toPrevious
+}
+
+func toPrevious(m *fsm) {
+	m.current, m.previous = m.previous, m.current
 }
 
 // Transition returns an action that transitions to a new state.
 func Transition(s State) Action {
 	return func(m *fsm) {
+		m.previous = m.current
 		m.current = s
 	}
 }
 
-// TransitionWith returns an action that transitions to a state that requires 1
+// TransitionWith1 returns an action that transitions to a state that requires 1
 // parameter.
-func TransitionWith[T any](
-	s func(context.Context, T) (Action, error),
-	v T,
+func TransitionWith1[T1 any](
+	s func(context.Context, T1) (Action, error),
+	v1 T1,
 ) Action {
 	return func(m *fsm) {
 		m.current = func(ctx context.Context) (Action, error) {
-			return s(ctx, v)
+			return s(ctx, v1)
 		}
 	}
 }
