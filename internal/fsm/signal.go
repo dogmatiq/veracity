f5bd1a523744029eb67bc9dadb5d62afc191201e
James Harris <contact@jamesharris.id.au>
2023-06-28 19:30:24 +1000

    Move signals into `fsm` package.

--- internal/fsm/signal.go
+++ internal/fsm/signal.go
@@ -1,9 +1,12 @@
-package signal
+package fsm
 
 import (
 	"sync"
 	"sync/atomic"
 )
+
+// CancelFunc is a function that cancels a watcher from receiving signals.
+type CancelFunc func()
 
 // Latch is signal that closes a set of "watcher channels" when it is notified.
 //
@@ -14,8 +17,6 @@ type Latch struct {
 	m        sync.Mutex
 	watchers map[chan<- struct{}]struct{}
 }
-
-var _ Signal = (*Latch)(nil)
 
 // Watch registers a watcher channel to be closed when the signal is notified.
 // If s has already been notified, the watcher is closed immediately.
@@ -58,5 +59,51 @@ func (s *Latch) Notify() {
 		}
 
 		s.watchers = nil
+	}
+}
+
+// Condition is a signal that sends a value to a set of buffered watcher channels
+// when it is notified.
+//
+// Any watchers that have a prior unhandled values are ignored.
+type Condition struct {
+	m        sync.RWMutex
+	watchers map[chan<- struct{}]struct{}
+}
+
+// Watch registers a buffered watcher channel to receive a value when the signal
+// is next notified.
+func (s *Condition) Watch(watcher chan<- struct{}) CancelFunc {
+	if cap(watcher) == 0 {
+		panic("channel must be buffered")
+	}
+
+	s.m.Lock()
+	defer s.m.Unlock()
+
+	if s.watchers == nil {
+		s.watchers = map[chan<- struct{}]struct{}{}
+	}
+
+	s.watchers[watcher] = struct{}{}
+
+	return func() {
+		s.m.Lock()
+		defer s.m.Unlock()
+		delete(s.watchers, watcher)
+	}
+}
+
+// Notify sends a value to each watcher that do not have a prior unhandled
+// notification.
+func (s *Condition) Notify() {
+	s.m.RLock()
+	defer s.m.RUnlock()
+
+	for ch := range s.watchers {
+		select {
+		case ch <- struct{}{}:
+		default:
+		}
 	}
 }
--- internal/signal/doc.go
+++
@@ -1,2 +0,0 @@
-// Package signal provides a set of channel-based signalling primitives.
-package signal
