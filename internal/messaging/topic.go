1d3b2defad527fc133e211ed04255b55b99225da
James Harris <contact@jamesharris.id.au>
2023-07-12 17:41:20 +1000

    Handle unsubscribes during publish.

--- internal/messaging/topic.go
+++ internal/messaging/topic.go
@@ -1,75 +1,84 @@
 package messaging
 
-import "sync"
+import (
+	"sync"
+
+	"golang.org/x/exp/slices"
+)
 
 // Topic is a source of messages of type T.
-type Topic[Message any] struct {
-	once       sync.Once
-	sub, unsub chan chan<- Message
-	pub        chan Message
-	done       chan struct{}
+type Topic[T any] struct {
+	init     sync.Once
+	messages chan T
+
+	m           sync.Mutex
+	subscribers []subscriber[T]
+}
+
+type subscriber[T any] struct {
+	messages     chan<- T
+	unsubscribed chan struct{}
 }
 
 // Subscribe registers ch as a subscriber to messages from this topic.
-func (t *Topic[Message]) Subscribe(ch chan<- Message) {
-	t.init()
-	select {
-	case <-t.done:
-	case t.sub <- ch:
-	}
+func (t *Topic[T]) Subscribe(ch chan<- T) {
+	t.m.Lock()
+	defer t.m.Unlock()
+
+	t.subscribers = append(
+		t.subscribers,
+		subscriber[T]{
+			messages:     ch,
+			unsubscribed: make(chan struct{}),
+		},
+	)
 }
 
-// Unsubscribe deregisters ch as a subscriber to messages from this topic.
-func (t *Topic[Message]) Unsubscribe(ch chan<- Message) {
-	t.init()
-	select {
-	case <-t.done:
-	case t.unsub <- ch:
+// Unsubscribe removes ch as a subscriber to messages from this topic.
+func (t *Topic[T]) Unsubscribe(ch chan<- T) {
+	t.m.Lock()
+	defer t.m.Unlock()
+
+	for i, sub := range t.subscribers {
+		if sub.messages == ch {
+			n := len(t.subscribers) - 1
+			t.subscribers[i] = t.subscribers[n]
+			t.subscribers[n] = subscriber[T]{}
+			t.subscribers = t.subscribers[:n]
+			close(sub.unsubscribed)
+		}
 	}
 }
 
 // Publish returns a channel that, when written, publishes a message to this
 // topic.
-func (t *Topic[Message]) Publish() chan<- Message {
-	t.init()
-	return t.pub
+func (t *Topic[T]) Publish() chan<- T {
+	t.init.Do(func() {
+		t.messages = make(chan T)
+		go t.run()
+	})
+	return t.messages
 }
 
 // Close closes the topic, preventing further messages from being published.
-func (t *Topic[Message]) Close() {
-	t.init()
-	close(t.done)
+func (t *Topic[T]) Close() {
+	t.init.Do(func() {
+		t.messages = make(chan T)
+	})
+	close(t.messages)
 }
 
-func (t *Topic[Message]) run() {
-	subscribers := map[chan<- Message]struct{}{}
+func (t *Topic[T]) run() {
+	for m := range t.messages {
+		t.m.Lock()
+		subscribers := slices.Clone(t.subscribers)
+		t.m.Unlock()
 
-	for {
-		select {
-		case ch := <-t.sub:
-			subscribers[ch] = struct{}{}
-		case ch := <-t.unsub:
-			delete(subscribers, ch)
-		case <-t.done:
-			return
-		case m := <-t.pub:
-			for sub := range subscribers {
-				select {
-				case <-t.done:
-					return
-				case sub <- m:
-				}
+		for _, sub := range subscribers {
+			select {
+			case sub.messages <- m:
+			case <-sub.unsubscribed:
 			}
 		}
 	}
-}
-
-func (t *Topic[Message]) init() {
-	t.once.Do(func() {
-		t.pub = make(chan Message)
-		t.sub = make(chan chan<- Message)
-		t.unsub = make(chan chan<- Message)
-		t.done = make(chan struct{})
-		go t.run()
-	})
 }
