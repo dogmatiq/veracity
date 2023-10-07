f194ba8d880685d5faa85675cb512d6ac3cb53f6
James Harris <contact@jamesharris.id.au>
2023-10-07 18:40:35 +1000

    Move `ScanFunc` into `scan.go`.

--- internal/protobuf/protojournal/scan.go
+++ internal/protobuf/protojournal/scan.go
@@ -5,7 +5,16 @@ import (
 
 	"github.com/dogmatiq/veracity/internal/protobuf/typedproto"
 	"github.com/dogmatiq/veracity/persistence/journal"
+	"google.golang.org/protobuf/proto"
 )
+
+// ScanFunc is a predicate function that produces a value of type T from a
+// record.
+type ScanFunc[T any, Record proto.Message] func(
+	ctx context.Context,
+	pos journal.Position,
+	rec Record,
+) (value T, ok bool, err error)
 
 // Scan finds a value within the journal by scanning all records
 // beginning with the record at the given position.
@@ -32,7 +41,7 @@ func Scan[
 }
 
 // ScanFromSearchResult finds a value within the journal by scanning all records
-// beginning with the for which cmp() returns true.
+// beginning with the record for which cmp() returns true.
 func ScanFromSearchResult[
 	T any,
 	Record typedproto.Message[Struct],
--- internal/protobuf/protojournal/search.go
+++ internal/protobuf/protojournal/search.go
@@ -9,26 +9,16 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-type (
-	// CompareFunc is a function that compares a record to some datum.
-	//
-	// If the record is less than the datum, cmp is negative. If the record is
-	// greater than the datum, cmp is positive. Otherwise, the record is
-	// considered equal to the datum.
-	CompareFunc[Record proto.Message] func(
-		ctx context.Context,
-		pos journal.Position,
-		rec Record,
-	) (cmp int, err error)
-
-	// ScanFunc is a predicate function that produces a value of type T from a
-	// record.
-	ScanFunc[T any, Record proto.Message] func(
-		ctx context.Context,
-		pos journal.Position,
-		rec Record,
-	) (value T, ok bool, err error)
-)
+// CompareFunc is a function that compares a record to some datum.
+//
+// If the record is less than the datum, cmp is negative. If the record is
+// greater than the datum, cmp is positive. Otherwise, the record is
+// considered equal to the datum.
+type CompareFunc[Record proto.Message] func(
+	ctx context.Context,
+	pos journal.Position,
+	rec Record,
+) (cmp int, err error)
 
 // Search performs a binary search of the journal to find the position of
 // the record for which cmp() returns zero.
